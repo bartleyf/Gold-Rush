d248e0b070e694a24373449ca077908ada7f896d
Frank Bartley <bartley.frank@gmail.com>
2017-11-15 17:58:09 -0600

    complete game

--- game.js
+++ game.js
@@ -1,52 +1,71 @@
 
-  var min = 19;
-  var max = 120
-  var targetNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-
-  $("#targetNumber").text(targetNumber);
-
+// number, win/lose counters
   var counter = 0;
   var wins = 0;
   var lose = 0;
 
-  // var numberOptions = Math.floor(Math.random() * 12) + 1;
+  $("#win").text(wins);
+  $("#lose").text(lose);
+
+  $("#start").on("click", function() {
+    start();
+  })
+
+function start() {
+  counter = 0;
+  // create target number
+  var min = 19;
+  var max = 120;
+  var targetNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  $("#targetNumber").text(targetNumber);
+
+// create array of numbers to be added to images
   var numberOptions = Array.from({length: 4}, () => Math.floor(Math.random() * 12) + 1);
   console.log(numberOptions);
 
+$("#images").empty()
   // for loop to create gold nuggets for every numberOption.
   for (var i = 0; i < numberOptions.length; i++) {
 
-    // For each iteration, create an image
+    //create an image
     var imageNugget = $("<img>");
 
-    imageNugget.addClass("image");
+    imageNugget.addClass("img-responsive col-md-3 image");
 
     // src link to the nugget image
     imageNugget.attr("src", "http://www.goldwhy.com/gold-images/gold-nugget.jpg");
 
-    // data attribute set equal to the array value.
+    // add data attribute set equal to the array value.
     imageNugget.attr("data-nuggetvalue", numberOptions[i]);
 
+    // select images section and add image
     $("#images").append(imageNugget);
-  }
+  }//end image loop
 
+// click event for iamges
   $(".image").on("click", function() {
 
     var nuggetValue = ($(this).attr("data-nuggetvalue"));
     nuggetValue = parseInt(nuggetValue);
 
+    // add number attacted to clicked image to value
     counter += nuggetValue;
-
+    // select score section and place score
     $("#score").text(counter)
 
+    // if else statement for win/lose message
     if (counter === targetNumber) {
       $("#score").text("YOU WIN!");
-      // $("#win").text(wins += 1);
+      wins += 1;
+      $("#win").text(wins);
+      start();
     }
-
-    else if (counter >= targetNumber) {
+    else if (counter > targetNumber) {
       $("#score").text("YOU LOSE!");
-      // $("#lose").text(lose += 1);
+      lose += 1;
+      $("#lose").text(lose);
+      start();
     }
-
-  });
+  });//end click event
+}
